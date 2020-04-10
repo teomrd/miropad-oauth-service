0f21ca7aa52e1e93ead20922b639261150a63143
teomrd <mironidis.gr@gmail.com>
2020-04-10 19:49:02 +0100

    Remove clutter, minimal express rest api

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .get('/', (req, res) => res.json({
+    hello: "world"
+  }))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
